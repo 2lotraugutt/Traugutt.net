e2fc659f983b3fd26e2fa8b75e7341b957abcf4e
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-08-25 02:56:02 +0200

    Sign out changes and fixes

--- app/auth/signout/page.tsx
+++ app/auth/signout/page.tsx
@@ -16,7 +16,7 @@ const poppingsFont500 = Poppins({
 	subsets: ["latin"],
 });
 
-export default function logout() {
+export default function Page() {
 	const { data: session, status } = useSession();
 	const { push } = useRouter();
 
@@ -31,15 +31,16 @@ export default function logout() {
 				<Image src="/Wave.svg" width="1920" height="1080" className="hidden xl:block absolute top-0 left-0 w-full h-full object-left object-cover" alt="" />
 			</div>
 
-			<div className="right-0 h-full absolute w-full xl:w-fit flex items-center justify-center xl:px-12 2xl:px-20">
-				<div className="p-4 md:p-20 text-center items-center flex flex-col gap-6 pb-20 md:pb-40  lg:pb-32">
-					<Image src="/Glasses.svg" width="250" height="100" alt="" />
+			<div className="p-4 md:p-20 text-center items-center flex flex-col gap-6 pb-12 md:pb-40 xl:gap-y-8 lg:pb-32">
+				<div className="p-4 md:p-20 text-center items-center flex flex-col gap-6 pb-12 md:pb-40 xl:gap-y-8 lg:pb-32">
+					<Image src="/Glasses.svg" width="250" height="100" alt="Glasses icon" />
 
-					<h1 className={`text-3xl sm:text-5xl ${poppingsFont700.className}`}>Sign out</h1>
-					<h2 className={`text-base sm:text-lg ${poppingsFont500.className}`}>Are you sure you want to sign out?</h2>
+					<h1 className={`text-3xl sm:text-5xl xl:text-6xl ${poppingsFont700.className}`}>Sign in</h1>
+					<h2 className={`-mt-3 text-base sm:text-lg xl:text-xl ${poppingsFont500.className}`}>We are happy to see you again!</h2>
+
 					<button
 						onClick={() => signOut({ callbackUrl: "/" })}
-						className={`text-lg sm:text-xl text-white bg-MainGreen rounded-full border-2 border-MainDarkGray/70 px-10 py-2 ${poppingsFont700.className}`}
+						className={`text-lg flex items-center sm:text-xl w-full text-white bg-MainDarkGray transition-all duration-300 rounded-full border-4 border-MainDarkGray hover:border-white/60 hover:bg-MainGreen px-10 py-2.5 ${poppingsFont700.className}`}
 					>
 						Sign out
 					</button>
