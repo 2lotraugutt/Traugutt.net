7df67e67c27961755d593ac56f32c74283404bb3
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-08-25 17:10:55 +0200

    Apos deleted from privacy policy

--- app/polityka-prywatnosci/page.tsx
+++ app/polityka-prywatnosci/page.tsx
@@ -125,7 +125,7 @@ export default function Page() {
 			<h3>Usage Data</h3>
 			<p>Usage Data is collected automatically when using the Service.</p>
 			<p>
-				Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service
+				Usage Data may include information such as Your Device&apos;s Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service
 				that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
 			</p>
 			<p>
@@ -145,10 +145,10 @@ export default function Page() {
 			</ul>
 			<p>
 				If You decide to register through or otherwise grant us access to a Third-Party Social Media Service, We may collect Personal data that is already associated
-				with Your Third-Party Social Media Service's account, such as Your name, Your email address, Your activities or Your contact list associated with that account.
+				with Your Third-Party Social Media Service&apos;s account, such as Your name, Your email address, Your activities or Your contact list associated with that account.
 			</p>
 			<p>
-				You may also have the option of sharing additional information with the Company through Your Third-Party Social Media Service's account. If You choose to
+				You may also have the option of sharing additional information with the Company through Your Third-Party Social Media Service&apos;s account. If You choose to
 				provide such information and Personal Data, during registration or otherwise, You are giving the Company permission to use, share, and store it in a manner
 				consistent with this Privacy Policy.
 			</p>
@@ -238,7 +238,7 @@ export default function Page() {
 				<li>
 					<p>
 						<strong>To contact You:</strong> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile
-						application's push notifications regarding updates or informative communications related to the functionalities, products or contracted services,
+						application&apos;s push notifications regarding updates or informative communications related to the functionalities, products or contracted services,
 						including the security updates, when necessary or reasonable for their implementation.
 					</p>
 				</li>
@@ -307,7 +307,7 @@ export default function Page() {
 			</p>
 			<h2>Transfer of Your Personal Data</h2>
 			<p>
-				Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing
+				Your information, including Personal Data, is processed at the Company&apos;s operating offices and in any other places where the parties involved in the processing
 				are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other
 				governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.
 			</p>
@@ -351,19 +351,19 @@ export default function Page() {
 				The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100%
 				secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.
 			</p>
-			<h1>Children's Privacy</h1>
+			<h1>Children&apos;s Privacy</h1>
 			<p>
 				Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If
 				You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected
 				Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.
 			</p>
 			<p>
-				If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's
+				If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent&apos;s
 				consent before We collect and use that information.
 			</p>
 			<h1>Links to Other Websites</h1>
 			<p>
-				Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's
+				Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party&apos;s
 				site. We strongly advise You to review the Privacy Policy of every site You visit.
 			</p>
 			<p>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
