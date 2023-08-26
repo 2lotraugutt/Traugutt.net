0ea978dca16aecf29e29d0a556325d5b002c9566
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-08-26 06:57:45 +0200

    Fixed dashboard routes

--- app/api/dashboard/posts/route.ts
+++ app/api/dashboard/posts/route.ts
@@ -8,26 +8,30 @@ export async function GET(request: NextRequest) {
 	const user = request.nextUrl.searchParams.get("user");
 
 	const session = (await getServerSession(authOptions)) as SessionDataType | undefined;
-	const role = session.user.role;
+	
+	if (session) {
+		const role = session.user.role;
 
-	if (user == undefined && !(role == "ADMIN" || role == "TEACHER" || role == "EDITOR"))
-		return NextResponse.json({ error: "You are not allowed to do this. Permissions exceeded" }, { status: 500 });
-	else {
-		if (role != "USER") {
-			const posts = await prisma.post.findMany({
-				take: count != 0 ? count : undefined,
-				orderBy: [
-					{
-						createdAt: "desc",
+		if (user == undefined && !(role == "ADMIN" || role == "TEACHER" || role == "EDITOR"))
+			return NextResponse.json({ error: "You are not allowed to do this. Permissions exceeded" }, { status: 500 });
+		else {
+			if (role != "USER") {
+				const posts = await prisma.post.findMany({
+					take: count != 0 ? count : undefined,
+					orderBy: [
+						{
+							createdAt: "desc",
+						},
+					],
+					where: {
+						authorId: user ?? undefined,
 					},
-				],
-				where: {
-					authorId: user ?? undefined,
-				},
-				include: { author: true },
-			});
+					include: { author: true },
+				});
 
-			return NextResponse.json(posts);
-		} else return NextResponse.json({ error: "You are not allowed to do this. Permissions exceeded" }, { status: 500 });
-	}
+				return NextResponse.json(posts);
+			} else return NextResponse.json({ error: "You are not allowed to do this. Permissions exceeded" }, { status: 500 });
+		}
+	} else return NextResponse.json({ error: "You are not logged in" }, { status: 500 });
+
 }
--- app/api/dashboard/publishPost/[id]/route.ts
+++ app/api/dashboard/publishPost/[id]/route.ts
@@ -7,14 +7,18 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
 	const status = request.nextUrl.searchParams.get("toggle") == "true" ? true : false;
 
 	const session = (await getServerSession(authOptions)) as SessionDataType | undefined;
-	const role = session.user.role;
+	
+	
+	if (session) {
+		const role = session.user.role;
 
-	if (role == "ADMIN" || role == "TEACHER" || role == "EDITOR") {
-		const post = await prisma.post.update({
-			where: { id: params.id },
-			data: { published: status },
-		});
+		if (role == "ADMIN" || role == "TEACHER" || role == "EDITOR") {
+			const post = await prisma.post.update({
+				where: { id: params.id },
+				data: { published: status },
+			});
 
-		return NextResponse.json(post.published);
-	} else return NextResponse.json({ error: "You are not allowed to do this. Permissions exceeded" }, { status: 500 });
+			return NextResponse.json(post.published);
+		} else return NextResponse.json({ error: "You are not allowed to do this. Permissions exceeded" }, { status: 500 });
+	} else return NextResponse.json({ error: "You are not logged in" }, { status: 500 });
 }
