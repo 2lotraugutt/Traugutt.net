1504f07e8269a11006e7c76dbdd218dfca098759
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-08-28 23:12:04 +0200

    New post - stage five

--- app/dashboard/post/page.tsx
+++ app/dashboard/post/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import StageFive from "@/components/newPostStages/stageFive";
 import StageFour from "@/components/newPostStages/stageFour";
 import StageOne from "@/components/newPostStages/stageOne";
 import StageThree from "@/components/newPostStages/stageThree";
@@ -13,12 +14,14 @@ export default function Page() {
 	const [image, setImage] = useState<File>();
 	const [imageName, setImageName] = useState("");
 	const [content, setContent] = useState("");
+	const [gallery, setGallery] = useState<{ name: string; image: File }[]>([]);
 
 	function stageUp() {
 		setStage((oldStage) => oldStage + 1);
 	}
 	function stageDown() {
-		setStage((oldStage) => oldStage - 1);
+		if (stage == 5 && content == "") setStage(3);
+		else setStage((oldStage) => oldStage - 1);
 	}
 
 	if (stage == 0) return <StageZero up={stageUp} />;
@@ -37,6 +40,11 @@ export default function Page() {
 	if (stage == 3) return <StageThree initContent={content} down={stageDown} up={stageUp} setContent={(content: string) => setContent(content)} />;
 	if (stage == 4) {
 		if (content != "") return <StageFour down={stageDown} up={stageUp} content={content} />;
-		else stageUp();
-	} else setStage(0);
+		else {
+			setStage(5);
+			return <></>;
+		}
+	}
+	if (stage == 5) return <StageFive down={stageDown} up={stageUp} setGallery={(gallery: { name: string; image: File }[]) => setGallery(gallery)} initGallery={gallery} />;
+	else setStage(0);
 }
---
+++ components/newPostStages/stageFive.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import Image from "next/image";
+import { Plus_Jakarta_Sans, Poppins } from "next/font/google";
+import { useState } from "react";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+	weight: "600",
+	subsets: ["latin"],
+});
+
+const poppingsFont700 = Poppins({
+	weight: "700",
+	subsets: ["latin"],
+});
+
+const poppingsFont600 = Poppins({
+	weight: "600",
+	subsets: ["latin"],
+});
+
+const poppingsFont400 = Poppins({
+	weight: "400",
+	subsets: ["latin"],
+});
+
+export default function StageFive(props: { down: Function; up: Function; setGallery: Function; initGallery: { name: string; image: File }[] }) {
+	const [gallery, setGallery] = useState<{ name: string; image: File }[]>(props.initGallery);
+	const [imageKey, setImageKey] = useState(1);
+
+	return (
+		<div className="dashboard-post-page">
+			<Image src="/newPost/Wave-4.svg" width="1920" height="1080" className="absolute top-0 right-0 w-full h-full object-center object-cover" alt="" />
+			<div className="flex flex-col items-center gap-y-10 absolute top-1/2 w-full -translate-y-1/2">
+				<div className="flex flex-col gap-y-3 max-w-xl">
+					<h1 className={`text-6xl text-center ${poppingsFont700.className}`}>Galleria zdjęć</h1>
+					<p className={`text-center px-5 text-lg ${poppingsFont400.className}`}>
+						<b className={`text-MainPurple ${poppingsFont600.className}`}>To już ostatni krok! </b>
+					</p>
+				</div>
+
+				<div className="grid grid-cols-3 gap-5 w-1/2 aspect-[3/2] overflow-y-auto p-3 border-2 border-dotted border-MainGreen rounded-lg">
+					<label>
+						<input
+							type="file"
+							hidden
+							onChange={({ target }) => {
+								if (target.files) {
+									const file = target.files[0];
+									setGallery((oldGallery) => {
+										console.log(oldGallery);
+										oldGallery.push({ name: URL.createObjectURL(file), image: file });
+										return oldGallery;
+									});
+									setImageKey((prev) => prev + 1);
+								}
+							}}
+						/>
+						<div
+							className={`w-full aspect-[25/16] rounded border-MainPurple flex items-center justify-center border-2 border-dashed cursor-pointer ${poppingsFont600.className}`}
+						>
+							Dodaj zdjęcie
+						</div>
+					</label>
+
+					{gallery.map((image, index) => (
+						<label key={index}>
+							<input
+								type="file"
+								hidden
+								onChange={({ target }) => {
+									if (target.files) {
+										const file = target.files[0];
+										setGallery((oldGallery) => {
+											oldGallery[index] = { name: URL.createObjectURL(file), image: file };
+											return oldGallery;
+										});
+										setImageKey((prev) => prev + 1);
+									}
+								}}
+							/>
+							<div className={`w-full rounded flex items-center justify-center cursor-pointer ${poppingsFont600.className}`}>
+								<img key={imageKey} src={image.name} className="object-cover aspect-[25/16]" alt="" />
+							</div>
+						</label>
+					))}
+				</div>
+
+				<div className="flex justify-between w-full max-w-xl">
+					<button onClick={() => props.down()} className={`bg-MainDarkGray px-8 py-3 text-white rounded-3xl ${plusJakartaSans.className}`}>
+						Powrót
+					</button>
+					<button onClick={() => props.up()} className={`bg-MainGreen px-8 py-3 text-white rounded-3xl ${plusJakartaSans.className}`}>
+						Kontynuuj
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
