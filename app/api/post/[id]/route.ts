958ccfe952fa6de77eae0a2d9a2667984eca05a3
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-08-30 01:39:33 +0200

    api/post/[id] protection

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,11 +1,22 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
-	const posts = await prisma.post.findUnique({
+	const session = (await getServerSession(authOptions)) as SessionDataType | undefined;
+
+	const post = await prisma.post.findUnique({
 		where: { id: params.id },
 		include: { author: true },
 	});
 
-	return NextResponse.json(posts);
+	if (post) {
+		if (post.published) return NextResponse.json(post);
+		else {
+			if (session && (session.user.role.managePosts || session.user.id == post.authorId)) {
+				return NextResponse.json(post);
+			}
+		}
+	} else return NextResponse.json({});
 }
