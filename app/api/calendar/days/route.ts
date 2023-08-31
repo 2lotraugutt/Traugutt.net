80958556a94f3b02881017fba0be0adf1a7a9d0d
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-08-31 21:38:49 +0200

    Calendar days api

---
+++ app/api/calendar/days/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request: NextRequest) {
+	const month = parseInt(request.nextUrl.searchParams.get("month") || "0");
+	const year = parseInt(request.nextUrl.searchParams.get("year") || "0");
+
+	const numbers = await prisma.day.findMany({
+		orderBy: [
+			{
+				timeStamp: "asc",
+			},
+		],
+		include: { events: true },
+		where: {
+			year: year != 0 ? year : undefined,
+			month: month != 0 ? month : undefined,
+		},
+	});
+
+	return NextResponse.json(numbers);
+}
