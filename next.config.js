e2abcbc2d34c671b8dd894bd34eddde3bfbfc67a
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-01 09:43:28 +0200

    Possible error fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
 	images: {
 		domains: ["2lo.traugutt.net", "lh3.googleusercontent.com"],
 	},
+	swcMinify: false, // it should be false by default
 };
 
 module.exports = nextConfig
