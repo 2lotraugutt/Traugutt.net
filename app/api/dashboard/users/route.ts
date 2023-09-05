4fed2d69d18259841b4f83e6a14cfeac3cb8a70e
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-05 12:14:15 +0200

    Fetching users

---
+++ app/api/dashboard/users/route.ts
@@ -0,0 +1,22 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+export async function GET(request: NextRequest) {
+	const count = parseInt(request.nextUrl.searchParams.get("count") || "0");
+
+	const session = (await getServerSession(authOptions)) as SessionDataType | undefined;
+
+	if (session) {
+		if (session.user.role.manageUsers) {
+			const users = await prisma.user.findMany({
+				take: count != 0 ? count : undefined,
+
+				include: { role: true },
+			});
+
+			return NextResponse.json(users);
+		} else return NextResponse.json({ error: "You are not allowed to do this. Permissions exceeded" }, { status: 500 });
+	} else return NextResponse.json({ error: "You are not logged in" }, { status: 500 });
+}
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { getSession } from "next-auth/react";
 import { Poppins } from "next/font/google";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+// import UserPostTile from "@/components/dashboard/userPostTile";
 
 const poppingsFont700 = Poppins({
 	weight: "700",
@@ -6,9 +12,47 @@ const poppingsFont700 = Poppins({
 });
 
 export default function Page() {
+	const [userSession, setSession] = useState<SessionDataType>();
+	const [users, setUsers] = useState<UserDataTypeWithRole[]>([]);
+	const [usersCount, setUsersCount] = useState<number>(1);
+	const router = useRouter();
+
+	useEffect(() => {
+		async function initFunction() {
+			const session = (await getSession()) as SessionDataType | undefined;
+
+			if (session) {
+				if (session.user.role.manageUsers) {
+					fetchUsers();
+					setSession(session);
+				} else router.push("/dashboard");
+			} else router.push("/");
+		}
+		initFunction();
+	}, []);
+
+	async function fetchUsers() {
+		const returnedPosts = await (await fetch(`/api/dashboard/users?count=${usersCount * 30}`)).json();
+		setUsers(returnedPosts);
+
+		setUsersCount((oldCount) => oldCount + 1);
+	}
+
 	return (
 		<div className="dashboard-page">
-			<h1 className={`dashboard-heading h-screen ${poppingsFont700.className}`}>Użytkownicy</h1>
+			<h1 className={`dashboard-heading ${poppingsFont700.className}`}>Użytkownicy</h1>
+
+			<div className="flex w-full flex-col gap-y-3 md:gap-2 lg:gap-3 xl:gap-4 4xl:gap-6">
+				{/* {users.map((userData: UserDataTypeWithRole) => (
+					<UserPostTile userData={userData} />
+				))} */}
+				<button
+					onClick={() => fetchUsers()}
+					className={`text-center h-fit w-full border-2 text-xl hover:bg-LightGray/20 transition-all duration-300 p-4 px-8 rounded-2xl ${poppingsFont700.className}`}
+				>
+					Załaduj więcej
+				</button>
+			</div>
 		</div>
 	);
 }
