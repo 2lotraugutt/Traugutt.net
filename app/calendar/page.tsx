3e0c746e7814a4188d2cb55be08fce5f250e70ea
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-07 10:16:16 +0200

    Switching months

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -3,6 +3,8 @@
 import CalendarComponent from "@/components/calendar/calendarComponent";
 import DatabaseTile from "@/components/calendar/databaseTile";
 import DayTile from "@/components/calendar/dayTile";
+import { faBackward, faForward } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { endOfMonth, getDate, getDaysInMonth, getISODay, getMonth, getYear, startOfMonth, startOfToday } from "date-fns";
 import { Poppins } from "next/font/google";
 import { useEffect, useState } from "react";
@@ -21,18 +23,40 @@ const poppingsFont500 = Poppins({
 });
 
 export default function Page() {
-	const today = startOfToday();
+	const [today, setToday] = useState<Date>(startOfToday());
+	const [month, setMonth] = useState<number>(getMonth(startOfToday()));
+	const [year, setYear] = useState<number>(getYear(startOfToday()));
 
-	const month = getMonth(today);
-	const year = getYear(today);
+	function changeMonth(up: boolean) {
+		console.log(month);
+		if (up) {
+			if (month == 11) {
+				setMonth(0);
+				setYear((old) => old + 1);
+			} else setMonth((old) => old + 1);
+			setToday(startOfMonth(new Date(year, month + 1, 2)));
+		} else {
+			if (month == 0) {
+				setMonth(11);
+				setYear((old) => old - 1);
+			} else setMonth((old) => old - 1);
+			setToday(startOfMonth(new Date(year, month - 1, 2)));
+		}
+	}
 
 	const weekDays = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"];
+	const monthsNames = ["Styczeń", "Luty", "Marzec", "Kwiecieć", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
 
 	return (
 		<div className="flex">
 			<div className="flex w-full flex-col p-12 gap-y-3 items-center">
 				<h1 className={`w-fit text-7xl mt-9 mb-20 ${poppingsFont700.className}`}>Kalendarz</h1>
-				<h3 className={`w-full text-left mb-2 text-5xl ${poppingsFont600.className}`}>Wrzesień</h3>
+
+				<div className="mb-2 flex items-center w-full gap-x-4 justify-start">
+					<FontAwesomeIcon icon={faBackward} className="text-MainDarkGray/80 h-5" onClick={() => changeMonth(false)} />
+					<h3 className={`text-left w-fit text-5xl ${poppingsFont600.className}`}>{monthsNames[month]}</h3>
+					<FontAwesomeIcon icon={faForward} className="text-MainDarkGray/80 h-5" onClick={() => changeMonth(true)} />
+				</div>
 
 				<div className="flex gap-x-6 text-MainDarkGray/60 w-full text-lg text-right pt-2 border-b-2">
 					{weekDays.map((weekDay) => (
--- components/calendar/calendarComponent.tsx
+++ components/calendar/calendarComponent.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function CalendarComponent(props: { today: Date; month: number; year: number }) {
 	const [days, setDays] = useState<DayDataTypeWithEvents[]>([]);
+	const [fetched, setFetched] = useState<boolean>(false);
 
 	const monthBegining = startOfMonth(props.today);
 	const monthEnding = endOfMonth(props.today);
@@ -17,39 +18,53 @@ export default function CalendarComponent(props: { today: Date; month: number; y
 	useEffect(() => {
 		fetchPost();
 		async function fetchPost() {
+			setFetched(false);
 			const days = await (await fetch(`api/calendar/days?month=${props.month}&year=${props.year}`)).json();
 
 			setDays(days);
+			setFetched(true);
 		}
 	}, [props.month, props.year]);
 
-	return (
-		<div className="grid grid-cols-7 w-full gap-6">
-			{[...Array(firstDayOfMonth)].map((e, i) => {
-				const dayNumber = prevMonthLastDay + (i - firstDayOfMonth + 1);
-				const date = new Date(props.year, props.month - 1, dayNumber);
+	if (fetched)
+		return (
+			<div className="grid grid-cols-7 w-full gap-6">
+				{[...Array(firstDayOfMonth)].map((e, i) => {
+					const dayNumber = prevMonthLastDay + (i - firstDayOfMonth + 1);
+					const date = new Date(props.year, props.month - 1, dayNumber);
 
-				return <DayTile differentMonth={true} date={date} key={date.toString()} />;
-			})}
+					return <DayTile differentMonth={true} date={date} key={date.toString()} />;
+				})}
+				{[...Array(monthLenght)].map((e, i) => {
+					const dayNumber = i + 1;
+					const filteredDays = days.filter((day) => day.day == dayNumber);
+					const day = filteredDays.length == 0 ? undefined : filteredDays[0];
 
-			{[...Array(monthLenght)].map((e, i) => {
-				const dayNumber = i + 1;
-				const filteredDays = days.filter((day) => day.day == dayNumber);
-				const day = filteredDays.length == 0 ? undefined : filteredDays[0];
+					if (day) return <DatabaseTile day={day} key={day.date} />;
+					else {
+						const date = new Date(props.year, props.month, dayNumber);
+						return <DayTile differentMonth={false} date={date} key={date.toString()} />;
+					}
+				})}
+				{[...Array(nextMonthDaysCount)].map((e, i) => {
+					const dayNumber = i + 1;
+					const date = new Date(props.year, props.month + 1, dayNumber);
 
-				if (day) return <DatabaseTile day={day} key={day.date} />;
-				else {
-					const date = new Date(props.year, props.month, dayNumber);
-					return <DayTile differentMonth={false} date={date} key={date.toString()} />;
-				}
-			})}
-
-			{[...Array(nextMonthDaysCount)].map((e, i) => {
-				const dayNumber = i + 1;
-				const date = new Date(props.year, props.month + 1, dayNumber);
-
-				return <DayTile differentMonth={true} date={date} key={date.toString()} />;
-			})}
-		</div>
-	);
+					return <DayTile differentMonth={true} date={date} key={date.toString()} />;
+				})}
+			</div>
+		);
+	else
+		return (
+			<div className="grid grid-cols-7 w-full gap-6">
+				{[...Array(35)].map((e, i) => {
+					return (
+						<div className={`aspect-[14/15] p-3.5 rounded-3xl animate-pulse ${i % 7 == 6 || i % 7 == 5 ? "bg-MediumGray/70" : "bg-MediumGray/40"}`}>
+							<div className={`p-1.5 text-center w-10 h-10 rounded-full text-xl bg-LightGray`}></div>
+						</div>
+					);
+				})}
+			</div>
+		);
 }
+
