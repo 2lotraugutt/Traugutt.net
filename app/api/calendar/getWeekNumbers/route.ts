0cd4979693c706818222aabc7c1794cf380c3d48
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-10 21:43:30 +0200

    Fixing errors

--- app/api/calendar/getWeekNumbers/route.ts
+++ app/api/calendar/getWeekNumbers/route.ts
@@ -1,18 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { zonedTimeToUtc } from "date-fns-tz";
-import { endOfWeek, isSaturday, isSunday, nextFriday, nextMonday, startOfToday, startOfWeek } from "date-fns";
+import { endOfWeek, isSaturday, isSunday, nextFriday, nextMonday, nextSaturday, nextSunday, startOfToday, startOfWeek } from "date-fns";
 
 export async function GET(request: NextRequest) {
 	const today = startOfToday();
 
 	let beginning = zonedTimeToUtc(startOfWeek(today), "UTC");
 	let ending = zonedTimeToUtc(endOfWeek(today), "UTC");
-
-	if (isSaturday(today) || isSunday(today)) {
-		beginning = zonedTimeToUtc(nextMonday(today), "UTC");
-		ending = zonedTimeToUtc(nextFriday(today), "UTC");
-	}
 
 	const numbers = await prisma.day.findMany({
 		orderBy: [
--- components/navigation/quick_menu_tiles/notificationsTile.tsx
+++ components/navigation/quick_menu_tiles/notificationsTile.tsx
@@ -5,7 +5,7 @@ export default function NotificationssTile() {
 		<div className="rounded-3xl relative md:col-span-1 row-span-2 xs:rounded-4xl bg-PurplePattern bg-cover bg-center">
 			<div className="absolute left-0 right-0 top-0 bottom-0 overflow-y-auto p-5">
 				{[...Array(5)].map((e, i) => (
-					<SingleNotificationTile />
+					<SingleNotificationTile key={i} />
 				))}
 			</div>
 		</div>
