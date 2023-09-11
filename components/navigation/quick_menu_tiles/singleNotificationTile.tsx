7d4de87fa2df943cb47cd65d8da750c87565fef8
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-11 08:45:01 +0200

    Notification tiles responsive

--- components/navigation/quick_menu_tiles/singleNotificationTile.tsx
+++ components/navigation/quick_menu_tiles/singleNotificationTile.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Poppins } from "next/font/google";
+import { useState } from "react";
 
 const poppingsFont600 = Poppins({
 	weight: "600",
@@ -11,10 +14,27 @@ const poppingsFont300 = Poppins({
 });
 
 export default function SingleNotificationTile() {
+	const [opened, setOpened] = useState(false);
+
 	return (
-		<div className="text-white group cursor-default duration-300 hover:bg-MainDarkGray/30 border-4 border-transparent hover:border-MainDarkGray/10 p-2.5 rounded-2xl">
-			<h1 className={"line-clamp-1 " + poppingsFont600.className}>Lorem, ipsum.</h1>
-			<p className={`relative after:text-white text-sm line-clamp-3 ${poppingsFont300.className}`}>
+		<div
+			onClick={() => setOpened((old) => !old)}
+			className={`text-white transition-all h-fit cursor-pointer duration-300 border-4 border-transparent p-1.5 sm:p-2 md:p-1.5 lg:p-2.5 3xl:p-3.5 rounded-2xl ${
+				opened ? "bg-MainDarkGray/30 border-MainDarkGray/10" : "hover:border-MainDarkGray/10 hover:bg-MainDarkGray/30"
+			}`}
+		>
+			<h1
+				className={`line-clamp-1 transition-all text-xs md:text-xs lg:text-sm xl:text-base 3xl:line-clamp-2 3xl:text-lg sm:text-sm duration-300 ${
+					opened ? "!line-clamp-none" : ""
+				} ${poppingsFont600.className}`}
+			>
+				Lorem, ipsum sit amet dolores quods antuson.
+			</h1>
+			<p
+				className={`relative after:text-white text-2xs md:text-2xs xl:text-sm sm:text-xs lg:text-xs 3xl:text-base line-clamp-3 3xl:line-clamp-4 4xl:line-clamp-5 transition-all duration-300 ${
+					opened ? "!line-clamp-none" : ""
+				} ${poppingsFont300.className}`}
+			>
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo inventore repudiandae possimus molestiae velit dolorem iste nostrum. Corporis sint ipsa rerum
 				officiis sunt perspiciatis ad placeat, exercitationem veritatis velit facilis?
 			</p>
