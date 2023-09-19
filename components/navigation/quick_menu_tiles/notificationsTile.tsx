07a5197af7c9eff1d6ac1acc155320ca7624b008
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-19 11:08:57 +0200

    Hide scroll bar

--- components/navigation/quick_menu_tiles/notificationsTile.tsx
+++ components/navigation/quick_menu_tiles/notificationsTile.tsx
@@ -18,7 +18,7 @@ export default function NotificationssTile() {
 
 	return (
 		<div className="rounded-3xl relative md:col-span-1 row-span-2 xs:rounded-4xl bg-PurplePattern bg-cover bg-center">
-			<div className="absolute whitespace-pre-wrap flex flex-col gap-0.5 lg:gap-1 left-0 right-0 top-0 bottom-0 overflow-y-auto p-2 xs:p-3 sm:p-4 md:p-2 lg:p-3.5 xl:p-5 3xl:p-7 4xl:p-9">
+			<div className="hide-scrollbar absolute whitespace-pre-wrap flex flex-col gap-0.5 lg:gap-1 left-0 right-0 top-0 bottom-0 overflow-y-auto p-2 xs:p-3 sm:p-4 md:p-2 lg:p-3.5 xl:p-5 3xl:p-7 4xl:p-9">
 				{notifications.map((notification, i) => (
 					<SingleNotificationTile notification={notification} key={i} />
 				))}
