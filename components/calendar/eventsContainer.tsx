fc5e56185f7f51006410755832db3db88b0e3915
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-21 14:28:53 +0200

    Event tiles responsive

--- components/calendar/eventsContainer.tsx
+++ components/calendar/eventsContainer.tsx
@@ -7,15 +7,23 @@ const poppingsFont500 = Poppins({
 
 export default function EventsContainer(props: { events: EventDataType[] }) {
 	return (
-		<div className="flex-col hidden md:flex w-full">
+		<div className="flex-col justify-end grow hidden md:flex w-full gap-0.5 xl:gap-1 3xl:gap-1.5">
 			{props.events.map((event, i) => {
+				let classes = "";
+
+				if (props.events.length == 1) classes = "line-clamp-2";
+				else if (props.events.length == 2) classes = "line-clamp-1 2xl:line-clamp-2";
+				else classes = "line-clamp-1";
+
 				return (
-					<div
+					<button
 						key={i}
-						className={`bg-MainPurple text-center text-2xs lg:text-xs xl:text-sm 3xl:text-base line-clamp-1 xl:line-clamp-2 py-1 px-2 xl:px-2.5 2xl:px-3 2xl:py-1.5 text-white rounded-lg ${poppingsFont500.className}`}
+						className={`event-container bg-MainPurple text-center text-2xs lg:text-xs xl:text-sm 2xl:text-sm 3xl:text-base py-[3px] px-1.5 xl:px-2.5 2xl:px-3 2xl:py-1.5 text-white rounded-lg xl:rounded-xl 
+						 ${classes}
+						 ${poppingsFont500.className}`}
 					>
 						{event.name}
-					</div>
+					</button>
 				);
 			})}
 		</div>
