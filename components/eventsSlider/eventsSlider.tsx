764fb328bed48214124d94288367eb8a8f0ca770
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-25 11:05:17 +0200

    Event slider design edits

--- components/eventsSlider/eventsSlider.tsx
+++ components/eventsSlider/eventsSlider.tsx
@@ -54,34 +54,37 @@ export default function EventsSlider() {
 
 	const monthsNames = ["Styczeń", "Luty", "Marzec", "Kwiecieć", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
 
-	return (
-		<div className="w-screen 4xl:w-full flex gap-x-7 overflow-x-auto lg:-mx-12 -mx-2 md:-mx-5 4xl:mx-0 lg:p-12 p-2 md:p-5 4xl:p-0">
-			{dates.map((date) => (
-				<div className="flex flex-col w-fit gap-y-3" key={date}>
-					<p className={`sticky left-0 w-fit text-base xs:text-lg lg:text-xl ${poppingsFont700.className}`}>
-						{monthsNames[parseInt(date.slice(0, 2)) - 1]} {date.slice(3, 8)}
-					</p>
+	if (dates.length != 0)
+		return (
+			<div className="w-screen 4xl:w-full flex gap-x-7 overflow-x-auto lg:-mx-12 -mx-2 md:-mx-5 4xl:mx-0 lg:px-12 px-2 md:px-5 4xl:px-0">
+				{dates.map((date) => (
+					<div className="flex flex-col w-fit gap-y-3" key={date}>
+						<p className={`sticky left-0 w-fit text-base xs:text-lg lg:text-xl ${poppingsFont700.className}`}>
+							{monthsNames[parseInt(date.slice(0, 2)) - 1]} {date.slice(3, 8)}
+						</p>
 
-					<div className="flex gap-x-[15px]">
-						{events
-							.filter((event) => event.date.includes(date))
-							.map((event) => (
-								<EventComponent event={event} key={event.id} />
-							))}
+						<div className="flex gap-x-[15px]">
+							{events
+								.filter((event) => event.date.includes(date))
+								.map((event) => (
+									<EventComponent event={event} key={event.id} />
+								))}
 
-						<button
-							onClick={() => setCount((old) => old + 1)}
-							className={`${events.length < count * 10 ? "hidden" : ""} flex rounded-3xl items-center border-[1px] border-MainPurple border-dotted p-7 gap-x-5`}
-						>
-							<p className={`text-2xl text-MainPurple bg-LightPurple rounded-full p-3.5 w-[60px] text-center ${poppingsFont700.className}`}>
-								<FontAwesomeIcon icon={faEllipsis} />
-							</p>
+							<button
+								onClick={() => setCount((old) => old + 1)}
+								className={`${
+									events.length < count * 10 ? "hidden" : ""
+								} flex rounded-3xl items-center border-[1px] border-MainPurple border-dotted p-7 gap-x-5`}
+							>
+								<p className={`text-2xl text-MainPurple bg-LightPurple rounded-full p-3.5 w-[60px] text-center ${poppingsFont700.className}`}>
+									<FontAwesomeIcon icon={faEllipsis} />
+								</p>
 
-							<p className={`whitespace-nowrap text-lg ${poppingsFont700.className}`}>Zobacz więcej</p>
-						</button>
+								<p className={`whitespace-nowrap text-lg ${poppingsFont700.className}`}>Zobacz więcej</p>
+							</button>
+						</div>
 					</div>
-				</div>
-			))}
-		</div>
-	);
+				))}
+			</div>
+		);
 }
