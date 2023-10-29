e175be615aa2b836753fff63616d2c4a41e75098
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-10-29 21:59:38 +0100

    Text edit

--- components/postStages/editPostStageZero.tsx
+++ components/postStages/editPostStageZero.tsx
@@ -14,11 +14,6 @@ const poppingsFont700 = Poppins({
 	subsets: ["latin"],
 });
 
-const poppingsFont600 = Poppins({
-	weight: "600",
-	subsets: ["latin"],
-});
-
 const poppingsFont400 = Poppins({
 	weight: "400",
 	subsets: ["latin"],
@@ -30,7 +25,7 @@ export default function EditPostStageZero(props: { up: Function }) {
 		<div className="dashboard-post-page">
 			<Image src="/newPost/Wave-0.svg" width="1920" height="1080" className="absolute top-0 right-0 w-full h-full object-center object-cover" alt="" />
 			<div className="flex flex-col gap-y-10 absolute top-1/2 max-w-xl -translate-y-1/2">
-				<h1 className={`text-3xl sm:text-4xl md:text-5xl xl:text-6xl text-center ${poppingsFont700.className}`}>Stworz nowy post</h1>
+				<h1 className={`text-3xl sm:text-4xl md:text-5xl xl:text-6xl text-center ${poppingsFont700.className}`}>Edytuj post!</h1>
 				<p className={`text-center px-5 text-xs xs:text-sm lg:text-base xl:text-xl 2xl:text-2xl ${poppingsFont400.className}`}>
 					Widzimi się już poraz kolejny. Mimo wszystko to bardzo dobrze, że edytujesz swoje posty! Dobrze jest cię tu widzieć ponownie. Idziemy dalej?
 				</p>
