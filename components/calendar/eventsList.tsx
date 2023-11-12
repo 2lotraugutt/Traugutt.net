a2de3396e8b1554bc3d73be50a4f4d9e9a06efa5
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-11-13 00:25:16 +0100

    Fixing mapping

--- components/calendar/eventsList.tsx
+++ components/calendar/eventsList.tsx
@@ -91,15 +91,15 @@ export default function EventsList() {
 		return (
 			<div className="flex flex-col w-3/5 mx-auto gap-y-3">
 				{[...Array(10)].map((n, i: number) => (
-					<div className={`flex rounded-3xl items-center border-[1px] border-SecondColor border-dotted p-3.5 lg:p-7 gap-x-3 sm:gap-x-4 lg:gap-x-5`}>
+					<div key={i} className={`flex rounded-3xl items-center border-[1px] border-SecondColor border-dotted p-3.5 lg:p-7 gap-x-3 sm:gap-x-4 lg:gap-x-5`}>
 						<div className={`w-[48px] animate-pulse sm:w-[52px] text-SecondColor bg-LightColor rounded-full lg:w-[60px] aspect-square`}></div>
 
 						<div className="flex flex-col gap-y-1.5">
 							<div className={`h-3 sm:h-5 lg:h-6 rounded-lg w-60 bg-LightGray`}></div>
 
 							<div className="flex gap-x-2">
-								{[...Array((i % 3) + 1)].map((e) => (
-									<div className={`flex h-fit rounded-3xl py-1 gap-x-2 px-2 sm:px-3 bg-LightGray/30 items-center`}>
+								{[...Array((i % 3) + 1)].map((m, j) => (
+									<div key={i * j} className={`flex h-fit rounded-3xl py-1 gap-x-2 px-2 sm:px-3 bg-LightGray/30 items-center`}>
 										<div className={`h-2 w-2 sm:h-3 sm:w-3 rounded-full bg-MainDarkGray/20`} />
 
 										<div className={`h-2 w-14 rounded-md bg-MainDarkGray/30 sm:h-2.5 md:h-3.5`}></div>
