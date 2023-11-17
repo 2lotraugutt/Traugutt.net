e0b6b0cbc420ed98f7644a603e8519b206a6ecbb
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-11-17 21:02:10 +0100

    Fixing tag api v2

--- app/api/calendar/tags/route.ts
+++ app/api/calendar/tags/route.ts
@@ -1,20 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/lib/prisma";
-import { getServerSession } from "next-auth";
 
 export async function GET(request: NextRequest) {
-	const session = (await getServerSession(authOptions)) as SessionDataType | undefined;
+	const tags = await prisma.eventTag.findMany({
+		orderBy: [
+			{
+				events: { _count: "desc" },
+			},
+		],
+	});
 
-	if (session) {
-		const tags = await prisma.eventTag.findMany({
-			orderBy: [
-				{
-					events: { _count: "desc" },
-				},
-			],
-		});
-
-		return NextResponse.json(tags);
-	} else return NextResponse.json({ error: "You are not logged in" }, { status: 500 });
+	return NextResponse.json(tags);
 }
