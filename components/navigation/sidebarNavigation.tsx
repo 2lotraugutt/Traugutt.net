0e33ec68277fdd13398254229b365709241ea66a
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-12-02 20:39:47 +0100

    "Matury" - sidebar

--- components/navigation/sidebarNavigation.tsx
+++ components/navigation/sidebarNavigation.tsx
@@ -1,11 +1,16 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
-import { faAngleRight, faClipboardList, faFileWord, faGraduationCap, faHouseUser, faPhone, faSchool, faShop } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight, faClipboardList, faFileWord, faGraduationCap, faHouseUser, faPersonChalkboard, faPhone, faSchool, faShop } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AnimatePresence, motion } from "framer-motion";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { useState } from "react";
 const poppingsFont600 = Poppins({
 	weight: "600",
+	subsets: ["latin"],
+});
+const poppingsFont500 = Poppins({
+	weight: "500",
 	subsets: ["latin"],
 });
 
@@ -19,12 +24,25 @@ export default function SidebarNavigation() {
 		setOpened(newOpened);
 	}
 
-	const mainNavs: { name: string; icon: IconDefinition }[] = [
-		{ name: "Szkoła", icon: faSchool },
-		{ name: "Dla ucznia", icon: faGraduationCap },
-		{ name: "Dla rodziców", icon: faHouseUser },
-		{ name: "Rekrutacja", icon: faClipboardList },
-		{ name: "Matura", icon: faFileWord },
+	const mainNavs: { name: string; icon: IconDefinition; pages: { link: string; name: string }[] }[] = [
+		{ name: "Szkoła", icon: faSchool, pages: [] },
+		{ name: "Dla ucznia", icon: faGraduationCap, pages: [] },
+		{ name: "Dla rodziców", icon: faHouseUser, pages: [] },
+		{ name: "Rekrutacja", icon: faClipboardList, pages: [] },
+		{
+			name: "Matura",
+			icon: faFileWord,
+			pages: [
+				{ link: "/page/wyniki-matur", name: "Wyniki matur" },
+				{ link: "/page/matura-miedzynarodowa", name: "Matura międzynarodowa" },
+				{ link: "https://cke.gov.pl/images/_KOMUNIKATY/20200820%20E8%20EM%20Komunikat%20o%20harmonogramie.pdf", name: "Harmonogram matury 2021" },
+				{ link: "https://cke.gov.pl/egzamin-maturalny/egzamin-w-nowej-formule/arkusze/", name: "Arkusze maturalne" },
+				{ link: "https://cke.gov.pl/images/_KOMUNIKATY/20200820%20EM%20Komunikat%20o%20dostosowaniach.pdf", name: "Dostosowania 2021" },
+				{ link: "/page/bibliografia", name: "Bibliografia" },
+				{ link: "https://cke.gov.pl/egzamin-maturalny/egzamin-w-nowej-formule/informatory/", name: "Informatory" },
+				{ link: "/page/autoprezentacja", name: "Autoprezentacja" },
+			],
+		},
 	];
 
 	return (
@@ -40,21 +58,42 @@ export default function SidebarNavigation() {
 
 			<div className="h-1 bg-DarkColor/30 rounded-lg"></div>
 
-			<div className="flex flex-col gap-y-1">
+			<motion.div className="flex flex-col gap-y-1">
 				{mainNavs.map((nav, i) => (
-					<button key={i} className={`sidebar-button ${openedList[i] && "bg-MainDarkGray/20"}`} onClick={() => changeState(i)}>
-						<FontAwesomeIcon icon={nav.icon} className="w-6 h-6 text-white py-3 px-4" />
-						<div className={`${poppingsFont600.className}`}>{nav.name}</div>
+					<>
+						<motion.button
+							key={i}
+							className={`sidebar-button ${openedList[i] && "bg-MainDarkGray/20"} ${poppingsFont600.className}`}
+							onClick={() => changeState(i)}
+						>
+							<FontAwesomeIcon icon={nav.icon} className="w-6 h-6 text-white py-3 px-4" />
+							<div>{nav.name}</div>
 
-						<FontAwesomeIcon icon={faAngleRight} className={`w-5 h-5 transition-all duration-300 ms-auto text-white px-4 ${openedList[i] && "rotate-90"}`} />
-					</button>
+							<FontAwesomeIcon icon={faAngleRight} className={`w-5 h-5 transition-all duration-300 ms-auto text-white px-4 ${openedList[i] && "rotate-90"}`} />
+						</motion.button>
+
+						<AnimatePresence>
+							{nav.pages.length != 0 && openedList[i] && (
+								<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="flex mt-1">
+									<div className="w-1 bg-MainColor mx-3 my-1 rounded-full"></div>
+									<div className="flex flex-col gap-y-1 grow">
+										{nav.pages.map((page, j) => (
+											<Link key={j} className={`sidebar-button px-3 py-1.5 w-full ${poppingsFont500.className}`} href={page.link}>
+												{page.name}
+											</Link>
+										))}
+									</div>
+								</motion.div>
+							)}
+						</AnimatePresence>
+					</>
 				))}
 
 				<Link href={""} className="sidebar-button">
 					<FontAwesomeIcon icon={faPhone} className="w-6 h-6 text-white py-3 px-4" />
 					<div className={`${poppingsFont600.className}`}>Kontakt</div>
 				</Link>
-			</div>
+			</motion.div>
 		</>
 	);
 }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withMDX = require("@next/mdx")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["2lo.traugutt.net", "lh3.googleusercontent.com", "cdn.discordapp.com"],
+		domains: ["2lo.traugutt.net", "lh3.googleusercontent.com", "cdn.discordapp.com", "traugutt.net"],
 	},
 	swcMinify: false, // it should be false by default
 };
