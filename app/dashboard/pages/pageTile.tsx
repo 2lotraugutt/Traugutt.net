b3dcf75a3b24973a98d863cb73409419d0e56e26
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-12-03 19:19:17 +0100

    Editing pages

--- app/dashboard/pages/pageTile.tsx
+++ app/dashboard/pages/pageTile.tsx
@@ -1,8 +1,11 @@
 import removeMarkdown from "@/lib/removeMarkdown";
-import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCancel, faCheck, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { MdEditor } from "md-editor-rt";
 import { Plus_Jakarta_Sans, Poppins } from "next/font/google";
 import { useState } from "react";
+import "md-editor-rt/lib/style.css";
+import { pages } from "next/dist/build/templates/app-page";
 
 const plusJakartaSansFont700 = Plus_Jakarta_Sans({
 	weight: "700",
@@ -21,6 +24,8 @@ const poppingsFont700 = Poppins({
 
 export default function PageTile(props: { pageData: { file: string; content: string }; refetchPages: Function }) {
 	const [deleteButtonText, setDeleteButtonText] = useState("Usuń podstronę");
+	const [newContent, setNewContent] = useState(props.pageData.content);
+	const [editing, setEditing] = useState(false);
 
 	async function deletePost() {
 		setDeleteButtonText("Usuwanie...");
@@ -35,20 +40,69 @@ export default function PageTile(props: { pageData: { file: string; content: str
 		props.refetchPages();
 	}
 
+	async function toggleEditing() {
+		if (editing) {
+			const data = new FormData();
+			data.set("content", newContent);
+
+			const res = await fetch(`/api/dashboard/pages/${props.pageData.file}`, {
+				method: "POST",
+				body: data,
+			});
+
+			if (!res.ok) throw new Error(await res.text());
+
+			if (res.ok) {
+				props.refetchPages();
+			}
+
+			setEditing(false);
+		} else {
+			setNewContent(props.pageData.content);
+			setEditing(true);
+		}
+	}
+
 	return (
 		<div className="h-fit w-full text-left flex-col xl:flex-row xl:items-center border-2 hover:bg-LightGray/40 transition-all duration-300 py-5 md:py-6 md:px-8 px-5 lg:py-8 lg:px-8 3xl:px-12 xl:py-9 flex gap-y-4 md:gap-y-6 lg:gap-y-10 xl:gap-x-10 rounded-2xl">
-			<div className="flex flex-col gap-y-2 xl:gap-y-5 lg:gap-y-3.5 w-full">
+			<div className="flex relative flex-col gap-y-2 xl:gap-y-5 lg:gap-y-3.5 w-full">
 				<p className={`line-clamp-2 md:line-clamp-none text-sm 2xs:text-lg xs:text-lg sm:text-xl md:text-2xl 4xl:text-3xl ${poppingsFont700.className}`}>
 					{props.pageData.file}
 				</p>
-				<p
-					className={`text-2xs 2xl:text-base text-MainDarkGray/50 2xs:text-xs xs:text-sm 3xl:line-clamp-5 4xl:line-clamp-6 line-clamp-4 ${poppingsFont400.className}`}
-				>
-					{removeMarkdown(props.pageData.content ?? "")}
-				</p>
+				{editing ? (
+					<MdEditor
+						modelValue={newContent}
+						onChange={setNewContent}
+						language="en-US"
+						noUploadImg
+						placeholder="Edytuj treść podstrony"
+						className="rounded-lg outline-none bg-white p-2 !h-[70vh] w-full"
+					/>
+				) : (
+					<p
+						className={`text-2xs 2xl:text-base text-MainDarkGray/50 2xs:text-xs xs:text-sm 3xl:line-clamp-5 4xl:line-clamp-6 line-clamp-4 ${poppingsFont400.className}`}
+					>
+						{removeMarkdown(props.pageData.content ?? "")}
+					</p>
+				)}
 			</div>
 
 			<div className="flex justify-between xl:justify-normal gap-y-2 2xl:gap-y-3 flex-col md:flex-row xl:flex-col gap-x-5">
+				<button onClick={() => toggleEditing()} className={`group/button dashboard-post-tile ${plusJakartaSansFont700.className}`}>
+					{editing ? "Potwierdź zmiany" : "Edytuj podstronę"}
+					<div className="dashboard-post-tile-icon">
+						<FontAwesomeIcon icon={editing ? faCheck : faPen} />
+					</div>
+				</button>
+				{editing && (
+					<button onClick={() => setEditing(false)} className={`group/button dashboard-post-tile ${plusJakartaSansFont700.className}`}>
+						Anuluj edycję
+						<div className="dashboard-post-tile-icon">
+							<FontAwesomeIcon icon={faCancel} />
+						</div>
+					</button>
+				)}
+
 				<button onClick={() => deletePost()} className={`group/button dashboard-post-tile ${plusJakartaSansFont700.className}`}>
 					{deleteButtonText}
 					<div className="dashboard-post-tile-icon">
