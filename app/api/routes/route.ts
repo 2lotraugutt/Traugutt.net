5b6c9d26a75268cbef4f777fe1169aedabd58544
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-12-05 18:45:53 +0100

    Minor amends

--- app/api/routes/route.ts
+++ app/api/routes/route.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET(request: NextRequest) {
-	const routes = await prisma.route.findMany();
+		const count = parseInt(request.nextUrl.searchParams.get("count") || "0");
+
+		const routes = await prisma.route.findMany({
+			take: count != 0 ? count : undefined,
+		});
 	return NextResponse.json(routes);
 }
