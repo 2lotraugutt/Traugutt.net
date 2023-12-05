42209d147d9ebdeec2531023e046692181f5dd86
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-12-05 10:28:03 +0100

    Fixing api issues with mdx

--- app/api/dashboard/pages/route.ts
+++ app/api/dashboard/pages/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import fs from "fs";
+import { promises as fsp } from "fs";
 
 export async function GET(request: NextRequest) {
 	const session = (await getServerSession(authOptions)) as SessionDataType | undefined;
@@ -13,7 +14,7 @@ export async function GET(request: NextRequest) {
 			const routes: string[] = fs.readdirSync("./content");
 
 			for (var route in routes) {
-				const parsedFile = await import("@/content/" + routes[route]);
+				const parsedFile = await fsp.readFile("./content/" + routes[route], "utf-8");
 				files.push({ file: routes[route], content: parsedFile });
 			}
 
