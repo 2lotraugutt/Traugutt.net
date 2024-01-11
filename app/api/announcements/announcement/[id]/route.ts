8a0de2b5b7b6fdbaf646177e67f7eb6a8fb502d9
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-01-11 01:23:48 +0100

    Single announcement - api

---
+++ app/api/announcements/announcement/[id]/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+	const id = params.id;
+
+	const announcement = await prisma.announcement.findFirst({
+		where: { id: id },
+	});
+
+	return NextResponse.json(announcement);
+}
