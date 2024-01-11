376413927813e15b5efb7d844503145e650f3df5
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-01-11 14:04:15 +0100

    API to pin notification

---
+++ app/api/dashboard/notifications/pinNotificaion/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+export async function POST(request: NextRequest) {
+	const session = (await getServerSession(authOptions)) as SessionDataType | undefined;
+
+	if (session) {
+		if (session.user.role.manageNotifications) {
+			const status = request.nextUrl.searchParams.get("toggle") == "true" ? true : false;
+			const data = await request.formData();
+
+			const id: string = data.get("id") as string;
+
+			await prisma.notification.update({
+				where: { id: id },
+				data: { pinned: status },
+			});
+
+			return NextResponse.json({ success: true });
+		} else return NextResponse.json({ error: "You are not allowed to do this. Permissions exceeded" }, { status: 500 });
+	} else return NextResponse.json({ error: "You are not logged in" }, { status: 500 });
+}
