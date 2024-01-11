394e84e4827aa875f24d0179da6c5f63768f9ac4
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-01-11 01:17:45 +0100

    Get announcements on date

---
+++ app/api/announcements/[date]/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request: NextRequest, { params }: { params: { date: string } }) {
+	const count = parseInt(request.nextUrl.searchParams.get("count") || "0");
+
+	const date = params.date;
+
+	const announcements = await prisma.announcement.findMany({
+		take: count != 0 ? count : undefined,
+		orderBy: [
+			{
+				createdAt: "desc",
+			},
+		],
+		where: { days: { every: { date: date } } },
+	});
+
+	return NextResponse.json(announcements);
+}
