3b077b340f96ea245a86d75771ba27ce36835075
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-01-11 01:09:07 +0100

    Get announcements

---
+++ app/api/announcements/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request: NextRequest) {
+	const count = parseInt(request.nextUrl.searchParams.get("count") || "0");
+
+	const announcements = await prisma.announcement.findMany({
+		take: count != 0 ? count : undefined,
+		orderBy: [
+			{
+				createdAt: "desc",
+			},
+		],
+	});
+
+	return NextResponse.json(announcements);
+}
