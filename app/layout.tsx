d1b10dbc3451af341682a0ddf211fe33deb296e5
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-01-21 20:35:36 +0100

    Fixing errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,25 +4,17 @@ import "./globals.css";
 import AuthProvider from "@/lib/authProvider";
 import "@/lib/generateNumbers";
 import { Metadata } from "next";
-import Head from "next/head";
 
 export const metadata: Metadata = {
 	title: "Traugutt",
 	description: "II Liceum Ogólnokształcące im. Romualda Traugutta w Częstochowie.",
+	keywords:
+		"II LO Traugutt, 2 LO Traugutt, Traugutt, Traugutt Częstochowa, Romuald Traugutt, Liceum Ogólnokształcące im. Romualda Traugutta, Edukacja, Nauka, Doskonałość Akademicka, Uczniowie, Program Nauczania, Działalność Pozalekcyjna, Wydarzenia Szkolne, Kadra Nauczycielska, Rekrutacja",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="pl">
-			<Head>
-				<meta
-					name="keywords"
-					content={
-						"II LO Traugutt, 2 LO Traugutt, Traugutt, Traugutt Częstochowa, Romuald Traugutt, Liceum Ogólnokształcące im. Romualda Traugutta, Edukacja, Nauka, Doskonałość Akademicka, Uczniowie, Program Nauczania, Działalność Pozalekcyjna, Wydarzenia Szkolne, Kadra Nauczycielska, Rekrutacja"
-					}
-				/>
-			</Head>
-
 			<AuthProvider>
 				<body className="max-w-screen-4xl mx-auto text-MainDarkGray">
 					<Navbar />
