efde8f5e12966bdf275f7d103642d6b999adddd7
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-04-04 09:22:07 +0200

    Add animations to search

--- components/searchbar/searchEventTile.tsx
+++ components/searchbar/searchEventTile.tsx
@@ -1,5 +1,16 @@
+import { motion } from "framer-motion";
+
 function SearchEventTile(props: { event: EventDataTypeWithPost }) {
-	return <div className="text-sm bg-white lg:text-base outline-none rounded-lg md:rounded-xl w-full p-1 md:p-2 lg:p-2.5">{props.event.name}</div>;
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			className="text-sm bg-white lg:text-base outline-none rounded-lg md:rounded-xl w-full p-1 md:p-2 lg:p-2.5"
+		>
+			{props.event.name}
+		</motion.div>
+	);
 }
 
 export default SearchEventTile;
--- components/searchbar/searchPostTile.tsx
+++ components/searchbar/searchPostTile.tsx
@@ -1,5 +1,16 @@
+import { motion } from "framer-motion";
+
 function SearchPostTile(props: { post: PostDataType }) {
-	return <div className="text-sm bg-white lg:text-base outline-none rounded-lg md:rounded-xl w-full p-1 md:p-2 lg:p-2.5">{props.post.title}</div>;
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			className="text-sm bg-white lg:text-base outline-none rounded-lg md:rounded-xl w-full p-1 md:p-2 lg:p-2.5"
+		>
+			{props.post.title}
+		</motion.div>
+	);
 }
 
 export default SearchPostTile;
