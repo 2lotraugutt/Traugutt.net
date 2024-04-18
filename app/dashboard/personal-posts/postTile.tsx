16a90fcf7bedbb2d6117db1594cbd33ddaa27989
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-04-18 21:23:05 +0200

    FInal post tiles design tweaks

--- app/dashboard/personal-posts/postTile.tsx
+++ app/dashboard/personal-posts/postTile.tsx
@@ -58,8 +58,10 @@ export default function DashboardPostTile(props: { postData: PostDataType; refet
 	return (
 		<div className="h-fit w-full text-left flex-col xl:flex-row xl:items-center border-2 hover:bg-LightGray/40 transition-all duration-300 py-5 md:py-6 md:px-8 px-5 lg:py-8 lg:px-8 3xl:px-12 xl:py-9 flex gap-y-4 md:gap-y-6 lg:gap-y-10 xl:gap-x-10 rounded-2xl">
 			<div className="flex xl:max-w-[36rem] 2xl:max-w-[47rem] 3xl:max-w-4xl 4xl:max-w-[72rem] flex-col gap-y-2 xl:gap-y-5 lg:gap-y-3.5 w-full">
-				<p className={`line-clamp-2 md:line-clamp-none text-sm 2xs:text-lg xs:text-lg sm:text-xl md:text-2xl 4xl:text-3xl items-center ${poppingsFont700.className}`}>
-					<span className="me-5">{props.postData.title}</span>
+				<p
+					className={`line-clamp-2 md:line-clamp-none text-sm 2xs:text-lg xs:text-lg sm:text-xl md:text-2xl 4xl:text-3xl flex items-center ${poppingsFont700.className}`}
+				>
+					<span className="me-4">{props.postData.title}</span>
 
 					<span
 						className={`px-5 hidden md:inline-block rounded-3xl text-sm lg:py-1 lg:px-7 flex-none text-center h-fit py-0.5 text-white ${
@@ -70,7 +72,7 @@ export default function DashboardPostTile(props: { postData: PostDataType; refet
 					</span>
 
 					{(props.postData.eventId || props.postData.pinned) && (
-						<div className="ms-5 hidden md:inline-flex items-center gap-4 xs:gap-5 bg-MainColor z-10 text-white w-fit text-xs xl:text-base 4xl:text-base 2xl:text-lg 3xl:px-6 rounded-2xl sm:py-1.5 lg:py-1.5 py-0.5 px-1.5 sm:px-3">
+						<div className="ms-2.5 hidden md:inline-flex items-center gap-4 xs:gap-5 bg-MainColor z-10 text-white w-fit text-xs xl:text-base 4xl:text-base 2xl:text-lg 3xl:px-6 rounded-2xl sm:py-1.5 lg:py-1.5 py-0.5 px-1.5 sm:px-3">
 							{props.postData.eventId && <FontAwesomeIcon icon={faLink} />}
 							{props.postData.pinned && <FontAwesomeIcon icon={faThumbTack} />}
 						</div>
--- app/dashboard/posts/adminPostTile.tsx
+++ app/dashboard/posts/adminPostTile.tsx
@@ -59,7 +59,7 @@ export default function DashboardPostTile(props: { postData: PostDataType; refet
 		const data = new FormData();
 		data.set("id", props.postData.id);
 
-		const post = await (
+		await(
 			await fetch(`/api/dashboard/posts/post/`, {
 				body: data,
 				method: "DELETE",
@@ -86,8 +86,10 @@ export default function DashboardPostTile(props: { postData: PostDataType; refet
 	return (
 		<div className="adminPostTile h-fit w-full text-left flex-col xl:flex-row xl:items-center border-2 hover:bg-LightGray/40 transition-all duration-300 py-5 md:py-6 md:px-8 px-5 lg:py-8 lg:px-8 3xl:px-12 xl:py-9 flex gap-y-4 md:gap-y-6 lg:gap-y-10 xl:gap-x-10 rounded-2xl">
 			<div className="flex xl:max-w-[35rem] 2xl:max-w-[46.5rem] 3xl:max-w-4xl 4xl:max-w-[69rem] flex-col gap-y-2 xl:gap-y-5 lg:gap-y-3.5 w-full">
-				<p className={`line-clamp-2 md:line-clamp-none text-sm 2xs:text-lg xs:text-lg sm:text-xl md:text-2xl 4xl:text-3xl ${poppingsFont700.className}`}>
-					<Link href={"/post/" + props.postData.id} className="me-5 hover:underline hover:text-DarkColor">
+				<p
+					className={`line-clamp-2 md:line-clamp-none text-sm 2xs:text-lg xs:text-lg sm:text-xl md:text-2xl 4xl:text-3xl flex items-center ${poppingsFont700.className}`}
+				>
+					<Link href={"/post/" + props.postData.id} className="me-4 hover:underline hover:text-DarkColor">
 						{props.postData.title}
 					</Link>
 
@@ -99,11 +101,11 @@ export default function DashboardPostTile(props: { postData: PostDataType; refet
 						{status ? "Publiczy" : "Nie publiczny"}
 					</span>
 
-					{(props.postData.eventId || props.postData.pinned) && (
-						<div className="ms-5 hidden md:inline-flex items-center gap-4 xs:gap-5 bg-MainColor z-10 text-white w-fit text-xs xl:text-base 4xl:text-base 2xl:text-lg 3xl:px-6 rounded-2xl sm:py-1.5 lg:py-1.5 py-0.5 px-1.5 sm:px-3">
+					{(props.postData.eventId || pinned) && (
+						<span className="ms-2.5 hidden md:inline-flex items-center gap-4 xs:gap-5 bg-DarkColor z-10 text-white w-fit text-xs xl:text-base 4xl:text-base 2xl:text-lg 3xl:px-6 rounded-2xl sm:py-1.5 lg:py-1.5 py-0.5 px-1.5 sm:px-3">
 							{props.postData.eventId && <FontAwesomeIcon icon={faLink} />}
-							{props.postData.pinned && <FontAwesomeIcon icon={faThumbTack} />}
-						</div>
+							{pinned && <FontAwesomeIcon icon={faThumbTack} />}
+						</span>
 					)}
 				</p>
 
