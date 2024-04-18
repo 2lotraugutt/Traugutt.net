88bd0fc9ddc5a6746173fa9abb8f35403cfcddb5
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-04-18 11:10:47 +0200

    Add post sitemap

---
+++ app/postsitemap/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const posts = (await (await fetch(`https://traugutt.net/api/posts`)).json()) as PostDataType[];
+	const postSites = posts.map((post) => {
+		return {
+			url: "https://traugutt.net/post/" + post.id,
+			lastModified: post.createdAt,
+			priority: 0.9,
+		};
+	});
+
+	return postSites.slice(500);
+}
