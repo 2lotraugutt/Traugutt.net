328e21d422084730c642a1e6c7c47464dcd9cb1a
Bartosz Wiaderek <64980866+WiaderekB@users.noreply.github.com>
2025-04-10 22:01:02 +0200

    refreshing to teacher retrieval API

--- app/api/teachers/route.ts
+++ app/api/teachers/route.ts
@@ -2,7 +2,10 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+	const count = parseInt(request.nextUrl.searchParams.get("count") || "0");
+
 	const teachers = await prisma.teacher.findMany({
+		take: count != 0 ? count : undefined,
 		orderBy: [
 			{ lastName: "asc" },
 			{ name: "asc" },
