54037a2a690c9613eaefc0066275c8935f9ac103
Bartosz Wiaderek <64980866+WiaderekB@users.noreply.github.com>
2025-04-11 22:17:47 +0200

    Fix DayTile stylink to ensure visibility on weekends

--- app/dashboard/numbers/dayTile.tsx
+++ app/dashboard/numbers/dayTile.tsx
@@ -7,7 +7,7 @@ const plusJakartaSans800 = Plus_Jakarta_Sans({
 
 export default function DayTile(props: { day: number; isWeekend: boolean }) {
 	return (
-		<div className={`day-tile !aspect-square lg:!aspect-[5/4] p-2 !hidden xs:!block ${props.isWeekend ? "bg-MediumGray/30" : "bg-MediumGray/10"} `}>
+		<div className={`day-tile !aspect-square lg:!aspect-[5/4] p-2 ${props.isWeekend ? "bg-MediumGray/30 !hidden xs:!block" : "bg-MediumGray/10"} `}>
 			<div
 				className={`day-number bg-LightGray ${!props.isWeekend ? "bg-LightGray text-MainDarkGray/50" : "bg-MediumGray text-MainDarkGray/70"} ${plusJakartaSans800.className}`}
 			>
