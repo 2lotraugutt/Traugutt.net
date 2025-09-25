1cefefc523e05b8a3802c0c5596c84b262aa1d60
Franciszek Skut <mgrkot@proton.me>
2025-09-25 12:08:44 +0200

    Fixed Ldap based auth

--- lib/ldap_search.sh
+++ lib/ldap_search.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
+#!/bin/sh
 set -e
 
-# Load the LDAP URL from .env file
-LDAP_URL=ldaps://traugutt.lan:636
+LDAP_URL=ldap://10.10.1.2
 
 # Ensure necessary arguments are provided
 if [ $# -ne 2 ]; then
@@ -12,21 +9,12 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
-# Command-line arguments
 LOGIN="$1"
 PASSWORD="$2"
-
-# Hardcoded base DN
 BASE_DN="DC=traugutt,DC=lan"
-
-# User search filter
 SEARCH_FILTER="(userPrincipalName=${LOGIN})"
 
-# LDAP search command with debug option (-d 1 for verbose output)
-RESULT=$(ldapsearch -x -H "$LDAP_URL" -D "$LOGIN" -w "$PASSWORD" -b "$BASE_DN" -LLL "$SEARCH_FILTER" -d 1)
-
-# Extract the first line containing the 'dn' attribute
-DN=$(echo "$RESULT" | grep "^dn:" | head -n 1)
-
-# Output the 'dn' line
-echo "$DN"
+ldapsearch -x -H "$LDAP_URL" \
+	 -D "$LOGIN" -w "$PASSWORD" \
+	 -b "$BASE_DN" -LLL "$SEARCH_FILTER" -d 1 \
+	| grep '^dn:' | head -n 1
